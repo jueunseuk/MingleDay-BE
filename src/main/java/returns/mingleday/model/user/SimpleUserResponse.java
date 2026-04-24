13a1905d1e8c62ce8009630dfc0214b4aca2f66c
jueunseuk <junsu120202@gmail.com>
2026-04-24 12:37:10 +0900

    fix: test api improve

---
+++ src/main/java/returns/mingleday/model/user/SimpleUserResponse.java
@@ -0,0 +1,10 @@
+package returns.mingleday.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SimpleUserResponse {
+    private Integer userId;
+}
