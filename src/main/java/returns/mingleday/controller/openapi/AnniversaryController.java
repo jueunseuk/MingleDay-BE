3abc6f22798d3d7e592748dc32f9622ca9da023c
jueunseuk <junsu120202@gmail.com>
2026-05-28 15:47:50 +0900

    feat: get spc days

--- src/main/java/returns/mingleday/controller/openapi/AnniversaryController.java
+++ src/main/java/returns/mingleday/controller/openapi/AnniversaryController.java
@@ -1,4 +1,4 @@
-package returns.mingleday.controller.anniversary;
+package returns.mingleday.controller.openapi;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -6,17 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import returns.mingleday.model.anniversary.AnniversaryItem;
+import returns.mingleday.service.openapi.AnniversaryService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/anniversary")
 public class AnniversaryController {
 
+    private final AnniversaryService anniversaryService;
+
     @GetMapping
-    public ResponseEntity<?> getAnniversary(
+    public ResponseEntity<List<AnniversaryItem>> getAnniversary(
             @RequestParam Integer year,
             @RequestParam Integer month
     ) {
-
+        List<AnniversaryItem> responses = anniversaryService.getAnniversary(year, month);
+        return ResponseEntity.ok(responses);
     }
 }
---
+++ src/main/java/returns/mingleday/service/openapi/AnniversaryService.java
@@ -0,0 +1,61 @@
+package returns.mingleday.service.openapi;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import returns.mingleday.model.anniversary.AnniversaryApiResponse;
+import returns.mingleday.model.anniversary.AnniversaryItem;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AnniversaryService {
+
+    @Value("${anniversary.endpoint}")
+    private String endpoint;
+
+    @Value("${anniversary.secret-key}")
+    private String secretKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public List<AnniversaryItem> getAnniversary(Integer year, Integer month) {
+        if(year == null) {
+            year = LocalDate.now().getYear();
+        }
+        if(month == null) {
+            month = LocalDate.now().getMonthValue();
+        }
+
+        log.info("Anniversary request occurred - year:{}, month:{}", year, month);
+
+        String url = endpoint
+                        + "?serviceKey=" + secretKey
+                        + "&solYear=" + year
+                        + "&solMonth=" + String.format("%02d", month);
+
+        log.info("Anniversary url: {}", url);
+
+        AnniversaryApiResponse response =
+                restTemplate.getForObject(
+                        url,
+                        AnniversaryApiResponse.class
+                );
+
+        log.info("Anniversary response: {}", response);
+
+        if(response == null || response.getBody() == null || response.getBody().getItems() == null) {
+            return Collections.emptyList();
+        }
+
+        return response.getBody()
+                .getItems()
+                .getItem();
+    }
+}
