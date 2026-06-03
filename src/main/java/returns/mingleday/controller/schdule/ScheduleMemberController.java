3f5778e58a3533161eb4cba04de9b49bb2e771e5
returns0825 <junsu120202@gmail.com>
2026-06-03 17:13:07 +0900

    feat: udpate schedule member memo

--- src/main/java/returns/mingleday/controller/schdule/ScheduleMemberController.java
+++ src/main/java/returns/mingleday/controller/schdule/ScheduleMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import returns.mingleday.flow.schedule.UpdateScheduleMemberFlow;
 import returns.mingleday.global.authentication.AuthUserDetail;
 import returns.mingleday.model.schedule.ScheduleMemberRequest;
+import returns.mingleday.model.schedule.UpdateScheduleMemberRequest;
 import returns.mingleday.response.success.SuccessResponse;
 
 import java.util.List;
@@ -19,13 +20,25 @@ public class ScheduleMemberController {
     private final UpdateScheduleMemberFlow updateScheduleMemberFlow;
 
     @PatchMapping
-    public ResponseEntity<SuccessResponse<String>> updateScheduleMember(
+    public ResponseEntity<SuccessResponse<String>> updateScheduleMembers(
         @AuthenticationPrincipal AuthUserDetail user,
         @PathVariable Integer mingleId,
         @PathVariable Long scheduleId,
         @RequestBody List<ScheduleMemberRequest> request
     ) {
-        updateScheduleMemberFlow.updateScheduleMember(user.getUserId(), mingleId, scheduleId, request);
+        updateScheduleMemberFlow.updateScheduleMembers(user.getUserId(), mingleId, scheduleId, request);
+        return ResponseEntity.ok(SuccessResponse.success("Success to update schedule member"));
+    }
+
+    @PatchMapping("/{scheduleMemberId}")
+    public ResponseEntity<SuccessResponse<String>> updateScheduleMember(
+            @AuthenticationPrincipal AuthUserDetail user,
+            @PathVariable Integer mingleId,
+            @PathVariable Long scheduleId,
+            @PathVariable Long scheduleMemberId,
+            @RequestBody UpdateScheduleMemberRequest request
+    ) {
+        updateScheduleMemberFlow.updateScheduleMember(user.getUserId(), mingleId, scheduleId, scheduleMemberId, request);
         return ResponseEntity.ok(SuccessResponse.success("Success to update schedule member"));
     }
 }
