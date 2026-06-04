2f284ee0a8c40b4ddf897af38a794cbedb5ca5f3
returns0825 <junsu120202@gmail.com>
2026-06-04 11:06:29 +0900

    fix: pagination

--- src/main/java/returns/mingleday/service/mingle/log/MingleLogService.java
+++ src/main/java/returns/mingleday/service/mingle/log/MingleLogService.java
@@ -30,7 +30,7 @@ public class MingleLogService {
         User user = userService.findUserByUserId(userId);
 
         List<Mingle> mingles = mingleMemberService.getAllMingle(user);
-        List<MingleLog> mingleLogs = mingleLogRepository.findAllByMingleInOrderByCreatedAt(mingles, PageableMaker.of("createdAt"));
+        List<MingleLog> mingleLogs = mingleLogRepository.findAllByMingleInOrderByCreatedAt(mingles, PageableMaker.of("createdAt", PageableMaker.DESC));
 
         return mingleLogs.stream().map(MyMingleLogResponse::new).toList();
     }
@@ -43,7 +43,7 @@ public class MingleLogService {
             throw new BaseException(GlobalExceptionCode.FORBIDDEN);
         }
 
-        List<MingleLog> mingleLogs = mingleLogRepository.findAllByMingleOrderByCreatedAt(mingle, PageableMaker.of("createdAt"));
+        List<MingleLog> mingleLogs = mingleLogRepository.findAllByMingleOrderByCreatedAt(mingle, PageableMaker.of("createdAt", PageableMaker.DESC));
 
         return mingleLogs.stream().map(MingleLogResponse::new).toList();
     }
