72be0eaaae94553689286b4b73b9ab55b9f44438
returns0825 <junsu120202@gmail.com>
2026-07-03 15:53:17 +0900

    fix: model field

--- src/main/java/returns/mingleday/model/schedule/SimpleScheduleInstanceResponse.java
+++ src/main/java/returns/mingleday/model/schedule/SimpleScheduleInstanceResponse.java
@@ -2,6 +2,7 @@ package returns.mingleday.model.schedule;
 
 import lombok.Data;
 import returns.mingleday.domain.schedule.ScheduleInstance;
+import returns.mingleday.domain.schedule.ScheduleStatus;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +12,7 @@ public class SimpleScheduleInstanceResponse {
     private Long scheduleInstanceId;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
+    private ScheduleStatus scheduleStatus;
 
     public SimpleScheduleInstanceResponse(ScheduleInstance scheduleInstance) {
         if (scheduleInstance == null) {
@@ -19,5 +21,6 @@ public class SimpleScheduleInstanceResponse {
         this.scheduleInstanceId = scheduleInstance.getScheduleInstanceId();
         this.startAt = scheduleInstance.getStartAt();
         this.endAt = scheduleInstance.getEndAt();
+        this.scheduleStatus = scheduleInstance.getScheduleStatus();
     }
 }
